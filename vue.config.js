f95af7519b0254ada6726d3fde833ad514b33f26
sudh-17 <395338095@qq.com>
2019-08-24 22:06:33 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ function resolve (dir) {
   return path.join(__dirname, '.', dir)
 }
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-antd-todomvc/dist/' : '/',
   chainWebpack: (config) => {
     config.resolve.alias
       .set("@", resolve("src"))
