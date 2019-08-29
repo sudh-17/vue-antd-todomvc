7492d3b45536e020a26c16d2c653a3b3ff15f814
sudh-17 <395338095@qq.com>
2019-08-29 10:12:30 +0800

    add github link

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <div class="menu">
-      <router-link to="/home">主页</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <div class="source">
-      <a href="https://github.com/sudh-17/vue-antd-todomvc/tree/master" target="_blank">源码地址</a>
+    <div class="header">
+      <div class="menu">
+        <router-link to="/home">
+          <a class="item" href="javascript:;">主页</a>
+        </router-link>|
+        <router-link to="/about">
+          <a class="item" href="javascript:;">About</a>
+        </router-link>
+      </div>
+      <a class="source" href="https://github.com/sudh-17/vue-antd-todomvc/tree/master" target="_blank">
+        <img src="./assets/github.png" alt srcset />
+      </a>
     </div>
     <router-view></router-view>
   </div>
@@ -18,18 +24,37 @@ export default {
 }
 </script>
 
-<style>
-.menu {
-  position: absolute;
-  top: 8px;
-  left: 19px;
-  font-size: 25px;
+<style lang="less" scoped>
+.header {
+  position: fixed;
+  top: 0px;
+  font-size: 20px;
   font-weight: bold;
+  width: 100%;
+  background: #1f1f1f;
+  padding: 3px 0;
+  .menu {
+    height: 27px;
+    display: inline-block;
+    width: 186px;
+    padding-left: 32px;
+    position: relative;
+    top: -3px;
+  }
+  .item {
+    color: aliceblue;
+  }
+  a {
+    text-decoration: none !important;
+  }
+  img {
+    width: 28px;
+    height: 28px;
+  }
+  .source {
+    position: relative;
+    top: 3px;
+  }
 }
-.source {
-  position: absolute;
-  left: 17px;
-  top: 100px;
-  font-size: 24px;
-}
+
 </style>
