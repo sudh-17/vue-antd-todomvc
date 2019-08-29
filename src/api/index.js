a15e82f76d3ace7078833fe8d8a3640d7ab84474
sudh-17 <395338095@qq.com>
2019-08-29 09:13:57 +0800

    add common.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,8 @@ import {
   updateTodo,
   completedAll,
   clearCompleted
-} from  './localStorage.js'  //'./todo.js'
+} from  './localStorage.js'  // 本地localStorage 数据接口
+//'./remoteStorage.js'// 远程MySql数据接口
 
 export {
   getAll,
